a9cddd3efb9c8f883b2db75e0c9ab318af82d275
Rasa <rasa.j1225@gmail.com>
2024-05-07 13:27:36 +0300

    show save button if logged in

--- lib/screens/ingredient_analysis.dart
+++ lib/screens/ingredient_analysis.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
@@ -13,9 +14,21 @@ class IngredientAnalysis extends StatefulWidget {
 class _IngredientAnalysisState extends State<IngredientAnalysis> {
   final ImagePicker _picker = ImagePicker();
   String _recognizedText = '';
-  bool _isLoading = false; // Loading state
-  List<dynamic> _allergens = []; // List of allergens
-  List<String> _fullIngredients = []; // Full ingredient list
+  bool _isLoading = false;
+  List<dynamic> _allergens = [];
+  List<String> _fullIngredients = [];
+  User? user;
+
+  @override
+  void initState() {
+    super.initState();
+    user = FirebaseAuth.instance.currentUser;
+    FirebaseAuth.instance.authStateChanges().listen((User? user) {
+      setState(() {
+        this.user = user;
+      });
+    });
+  }
 
   Future<void> _processImage(XFile image) async {
     try {
@@ -99,64 +112,101 @@ class _IngredientAnalysisState extends State<IngredientAnalysis> {
     }
   }
 
+  void _saveProduct() {
+    print('Product saved: ${_fullIngredients.join(', ')}');
+    // Implement actual save logic here
+  }
+  @override
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: _isLoading
           ? Center(child: CircularProgressIndicator())
-          : (_recognizedText.isEmpty && _allergens.isEmpty && _fullIngredients.isEmpty)
-          ? Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Select an image to analyze the ingredients.'),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                ElevatedButton(
-                  onPressed: _openCamera,
-                  child: Text('Open Camera'),
-                  style: ElevatedButton.styleFrom(primary: Colors.blue),
-                ),
-                ElevatedButton(
-                  onPressed: _openGallery,
-                  child: Text('Upload from Files'),
-                  style: ElevatedButton.styleFrom(primary: Colors.blue),
-                ),
-              ],
-            ),
-          ],
-        ),
-      )
-          : Padding(
-        padding: const EdgeInsets.only(top: 30.0),
-        child: ListView(
-          children: <Widget>[
-            // Check for empty allergens list
-            Card(
-              child: ListTile(
-                contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
-                title: Text('Potential Allergens', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                subtitle: _allergens.isEmpty
-                    ? Text('There are no potential allergens in this product')
-                    : Text(_allergens.map((a) => a['ingredient']).join(', ')),
+          : buildContent(),
+    );
+  }
+
+  Widget buildContent() {
+    if (_recognizedText.isEmpty && _allergens.isEmpty && _fullIngredients.isEmpty) {
+      return buildInitialContent();
+    } else {
+      return buildAnalysisContent();
+    }
+  }
+
+  Widget buildInitialContent() {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('Select an image to analyze the ingredients.'),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: _openCamera,
+                child: Text('Open Camera'),
+                style: ElevatedButton.styleFrom(primary: Colors.blue),
               ),
-            ),
-            if (_fullIngredients.isNotEmpty)
-              Card(
-                child: ListTile(
-                  contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 10.0),
-                  title: Text('Full Ingredients', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                  subtitle: Text(_fullIngredients.join(', ')),
-                ),
+              ElevatedButton(
+                onPressed: _openGallery,
+                child: Text('Upload from Files'),
+                style: ElevatedButton.styleFrom(primary: Colors.blue),
               ),
-          ],
-        ),
+            ],
+          ),
+        ],
       ),
     );
   }
 
+  Widget buildAnalysisContent() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 30.0, bottom: 30.0, left: 10.0, right: 10.0),
+      child: ListView(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              title: Text('Potential Allergens', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              subtitle: _allergens.isEmpty
+                  ? Text('There are no potential allergens in this product')
+                  : Text(_allergens.map((a) => a['ingredient']).join(', ')),
+            ),
+          ),
+          if (_fullIngredients.isNotEmpty)
+            Card(
+              child: ListTile(
+                title: Text('Full Ingredients', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+                subtitle: Text(_fullIngredients.join(', ')),
+              ),
+            ),
+          if (user != null)
+            ElevatedButton(
+              onPressed: _saveProduct,
+              child: Text('Save Product'),
+              style: ElevatedButton.styleFrom(primary: Colors.blue),
+            ),
+          OutlinedButton(
+            onPressed: _openCamera,
+            child: Text('Open Camera'),
+            style: ElevatedButton.styleFrom(primary: Colors.white),
+          ),
+          OutlinedButton(
+            onPressed: _openGallery,
+            child: Text('Upload from Files'),
+            style: ElevatedButton.styleFrom(primary: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
 
+  @override
+  void dispose() {
+    //_emailController.dispose();
+    //_passwordController.dispose();
+    super.dispose();
+  }
 
 }
